52f4db9e66bed138379ce897a47013114decc6a3
Denis Nishida <denis.nishida@gmail.com>
2017-11-02 19:34:52 -0500

    Created layout and added internet connection check and api processor

--- app/src/main/java/com/example/winnipegtransitdemo/MainActivity.java
+++ app/src/main/java/com/example/winnipegtransitdemo/MainActivity.java
@@ -1,7 +1,21 @@
 package com.example.winnipegtransitdemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -11,5 +25,106 @@ public class MainActivity extends AppCompatActivity
   {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    processRss();
+  }
+
+  private boolean isNetworkAvailable()
+  {
+    ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+    NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+    return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+  }
+
+  public void processRss()
+  {
+    if (isNetworkAvailable())
+    {
+      // create and execute AsyncTask
+      RssProcessingTask task = new RssProcessingTask();
+      task.execute();
+    }
+    else
+    {
+      Toast.makeText(this, "No internet connection", Toast.LENGTH_LONG).show();
+    }
+  }
+
+  class RssProcessingTask extends AsyncTask
+  {
+    // On the start of the thread
+    @Override
+    protected void onPreExecute()
+    {
+      super.onPreExecute();
+    }
+
+    @Override
+    protected Object doInBackground(Object[] objects)
+    {
+      // Create URL object to RSS file
+      URL url = null;
+      String feedUrl = "https://api.winnipegtransit.com/v2/statuses/schedule.json?api-key=rQ8lXW4lpLR9CwiYqK";
+
+      try
+      {
+        url = new URL(feedUrl);
+      }
+      catch (MalformedURLException e)
+      {
+        e.printStackTrace();
+      }
+
+      // Create and open HTTP connection
+      HttpURLConnection connection = null;
+      try
+      {
+        connection = (HttpURLConnection) url.openConnection();
+      }
+      catch (IOException e)
+      {
+        e.printStackTrace();
+      }
+
+      try
+      {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        StringBuilder stringBuilder = new StringBuilder();
+        String line;
+        while ((line = bufferedReader.readLine()) != null)
+        {
+          stringBuilder.append(line);
+        }
+        bufferedReader.close();
+        return stringBuilder.toString();
+      }
+      catch (IOException e)
+      {
+        e.printStackTrace();
+      }
+      finally
+      {
+        connection.disconnect();
+      }
+
+
+      return null;
+    }
+
+    // On the end of the thread
+
+    @Override
+    protected void onPostExecute(Object o)
+    {
+//      super.onPostExecute(o);
+//      if (response == null)
+//      {
+//        response = "THERE WAS AN ERROR";
+//      }
+      Log.i("INFO", o.toString());
+
+      TextView textView = (TextView) findViewById(R.id.textView);
+      textView.setText(o.toString());
+    }
   }
 }
